e452a00163d35172a5df952505bcedbb67a3a2a7
kupets <kupets_work@mil.ru>
2025-09-13 15:22:00 +0300

    +

--- base/urls.py
+++ base/urls.py
@@ -17,5 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from api.models import CourseResource, CategoryResource
 
-urlpatterns = [path("admin/", admin.site.urls), path("shop/", include("shop.urls"))]
+course_resource = CourseResource()
+category_resource = CategoryResource()
+
+urlpatterns = [
+    path("admin/", admin.site.urls), 
+    path("shop/", include("shop.urls")),
+    path("api/", include(course_resource.urls)),
+    path("api/", include(category_resource.urls))
+]
--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+app_name = "shop"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("course/<int:course_id>", views.single_course, name="single_course"),
+]
